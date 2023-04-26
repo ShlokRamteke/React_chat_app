c552e3aecbfeaa506a30f0d1d166557792ad968a
krt <52488299+ShlokRamteke@users.noreply.github.com>
2023-04-26 18:36:05 +0530

    done with the project however the sign-in dosn't work

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
+import {auth, db} from "../firebase";
+import{addDoc, collection, serverTimestamp} from "firebase/firestore"
 
-const SendMessage = () => {
+
+const SendMessage = ({scroll}) => {
+  const [message, setMessage] =useState("");
+
+  const SendMessage = async (event) => {
+    event.preventDefault();
+    if (message.trim() === "") {
+      alert("Enter valid message");
+      return;
+    }
+    const { uid, displayName, photoURL } = auth.currentUser;
+    await addDoc(collection(db, "messages"), {
+      text: message,
+      name: displayName,
+      avatar: photoURL,
+      createdAt: serverTimestamp(),
+      uid,
+    });
+    setMessage("");
+    scroll.current.scrollIntoView({behavior:"smooth"});
+  };
   return (
-    <form className="send-message">
+    <form onsubmit={(event)=>SendMessage(event)} className="send-message">
       <label htmlFor="messageInput" hidden>
         Enter Message
       </label>
