bb5db2c2f57513e57aeb4a4f87df7db7a051f357
krt <52488299+ShlokRamteke@users.noreply.github.com>
2023-04-26 17:17:50 +0530

    Added App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
 import "./App.css";
 import NavBar from "./components/NavBar"
-import ChatBoz from ""
+import ChatBox from "./components/ChatBox"
+import Welcome from "./components/Welcome"
+import { useState } from "react";
+
+function App() {
+    const [user, setUser]=useState(false);
+
+    return(
+        <div className="App">
+            <NavBar />
+            {!user ? (
+                <Welcome />
+            ) : (
+                <>
+                <ChatBox />
+                </>
+            )}
+        </div>
+    );
+}
+export default App;
